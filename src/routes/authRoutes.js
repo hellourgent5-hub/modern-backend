4c066b380820962a4c67e6b4d2bc7b264fb21cb6
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 13:23:41 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,10 +3,7 @@ import { registerUser, loginUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// POST /api/auth/register
 router.post('/register', registerUser);
-
-// POST /api/auth/login
 router.post('/login', loginUser);
 
 export default router;
