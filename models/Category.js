9e89d10c9d64edea915da72eaf7ac473f36c3117
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 13:59:37 +0530

    Create Category.js

---
+++ models/Category.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  module: { type: mongoose.Schema.Types.ObjectId, ref: "Module", required: true }
+}, { timestamps: true });
+
+export default mongoose.model("Category", categorySchema);
