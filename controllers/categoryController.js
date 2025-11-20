a7bccada264bfb6e1e4a0afde7b454ec0cf45b97
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 16:32:40 +0530

    Create categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,21 @@
+import Category from "../models/Category.js";
+
+// Get all categories
+export const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create category
+export const createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
